5bd8a75a35fae0b25380a804c393ed34d9ba50c7
Dennis Mbugua <dennis@starlaunch.org>
2022-04-01 10:13:02 +0300

    CRUD functionality for a ToDo app in reactjs

---
+++ frontend/src/components/Modal.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react'
+import {Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Input, Label} from "reactstrap"
+
+export default class CustomModal extends Component{
+    constructor(props){
+        super(props)
+        this.state = {activeItem: this.props.activeItem}
+    }
+    handleChange = (e) => {
+        let {name, value} = e.target
+        if (e.target.type === "checkbox"){
+            value = e.target.checked
+        }
+        const activeItem = { ...this.state.activeItem, [name]: value}
+        this.setState({activeItem})
+    }
+    render(){
+        const {toggle, onSave} = this.props
+        return(
+            <Modal isOpen={true} toggle={toggle}>
+                <ModalHeader toggle={toggle}>ToDo Item</ModalHeader>
+                <ModalBody>
+                    <Form>
+                        <FormGroup>
+                            <label>Title</label>
+                            <input type='text' id='todo-title' name='title' value={this.state.activeItem.title} onChange={this.handleChange} placeholder='Enter ToDo Title' />
+                        </FormGroup>
+                        <FormGroup>
+                            <label for='todo-description'>Description</label>
+                            <input type='text' id='todo-description' name='description' value={this.state.activeItem.description} onChange={this.handleChange} placeholder='Enter ToDo Description' />
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <input type='checkbox' name='completed' checked={this.state.activeItem.completed} onChange={this.handleChange} />
+                            Completed</Label>
+                        </FormGroup>
+                    </Form>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color='success' onClick={() => onSave(this.state.activeItem)}>Save</Button>
+                </ModalFooter>
+            </Modal>
+        )
+    }
+}
